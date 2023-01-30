7a50aa6c7216b209a5531abc2e9d981b63bcec97
MRXAZK <mrxazk@protonmail.com>
2023-01-29 22:22:43 -0500

    Add Authorization

--- src/components/Layouts/MainHeader.vue
+++ src/components/Layouts/MainHeader.vue
@@ -402,7 +402,7 @@ const { users } = storeToRefs(usersStore);
 
 
 
-usersStore.afterLogin();
+usersStore.getAll();
 </script>
 
 <script>
--- src/stores/users.store.js
+++ src/stores/users.store.js
@@ -1,7 +1,7 @@
 // stores/users.store.js
 import { defineStore } from "pinia";
 import axios from "axios";
-import { useAuthStore } from "./auth.store";
+import Cookies from "js-cookie";
 
 export const useUsersStore = defineStore({
   id: "users",
@@ -10,16 +10,13 @@ export const useUsersStore = defineStore({
   }),
   actions: {
     async getAll() {
+      axios.defaults.headers.common["Authorization"] =
+        "Bearer " + Cookies.get("access_token");
       this.users = { loading: true };
       await axios
         .get("api/users/me")
         .then((users) => (this.users = users.data.user))
         .catch((error) => (this.users = { error }));
-    },
-    async afterLogin() {
-      if (useAuthStore().logged_in) {
-        this.getAll();
-      }
     },
   },
 });
